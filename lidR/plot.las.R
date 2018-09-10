101b8ffdfae5a09634aae6ef88f872443b6070b5
dragonflystats <statslabdublin@gmail.com>
2018-09-10 12:09:40 +0100

    reorganize repos

--- lidR/plot.las.R
+++
@@ -1,8 +0,0 @@
-LASfile <- system.file("extdata", "Megaplot.laz", package="lidR")
-lidar = readLAS(LASfile)
-
-plot(lidar)
-
-# Outliers of intensity breaks the color range. Use the trim parameter.
-plot(lidar, color = "Intensity", colorPalette = heat.colors(50))
-plot(lidar, color = "Intensity", colorPalette = heat.colors(50), trim = 0.99)
